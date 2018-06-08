9e0c0c38a670f3bf04122bbd01fac1f392634f30
Shiffler <ajshiff@byui.edu>
2018-06-08 13:55:14 -0600

    Prep for implementing solution to async-you_e01: Waterfall

--- async-you_e01.js
+++ async-you_e01.js
@@ -2,59 +2,59 @@
  * WATERFALL
  **********************************************************/
 var async = require('async');
-// const http = require('http');
-// const fs = require('fs');
-// const filepath = process.argv[2];
-// console.log(filepath);
+const http = require('http');
+const fs = require('fs');
+const filepath = process.argv[2];
+console.log(filepath);
 
-var one = 'one',
-    two = 'two';
+readFile = function (callback) {
+    fs.readFile(filepath);
+    callback(null, one, two)
+};
 
-function myFristFunction(callback) {
-    console.log('1: Start');
-    callback(null, one, two);
-}
 
-function mySecondFunction(won, too, callback) {
-    console.log('2 won: ' + won);
-    console.log('2 too: ' + too);
-    let wontoo = won + too;
-    callback(null, wontoo);
-}
-
-function myThirdFunction(juan, callback) {
-    console.log('3 juan: ' + juan);
-    callback(null, juan);
-}
-
-function myFinalExecution(err, finalOutput) {
-    if (err) {
-        console.log('error: ' + err);
-    }
-    console.log('Result: ' + finalOutput);
-}
-
-/*
 async.waterfall([
-    function (callback) {
-        // let url = '';
-        fs.readFile(filepath, 'utf8', function doneReading(err, fileContents) {
-            // if (err) {throw err;}
-            // console.log(fileContents);
-            // url = fileContents;
-            callback(fileContents);
-        });
-        //callback(url);
-    },
-    function (url) {
-        console.log(url);
-    }
-
+    readFile
 ]);
-*/
 
-async.waterfall([
-    myFristFunction,
-    mySecondFunction,
-    myThirdFunction
-], myFinalExecution);
+
+/*********************************************************************
+ * ===============================START-------------------------------
+ * Below is code for experimenting to understand how waterfall behaves
+ *********************************************************************/
+// var one = 'one',
+//     two = 'two';
+
+// function myFristFunction(callback) {
+//     console.log('1: Start');
+//     callback(null, one, two);
+// }
+
+// function mySecondFunction(won, too, callback) {
+//     console.log('2 won: ' + won);
+//     console.log('2 too: ' + too);
+//     let wontoo = won + too;
+//     callback(null, wontoo);
+// }
+
+// function myThirdFunction(juan, callback) {
+//     console.log('3 juan: ' + juan);
+//     callback(null, juan);
+// }
+
+// function myFinalExecution(err, finalOutput) {
+//     if (err) {
+//         console.log('error: ' + err);
+//     }
+//     console.log('Result: ' + finalOutput);
+// }
+
+// async.waterfall([
+//     myFristFunction,
+//     mySecondFunction,
+//     myThirdFunction
+// ], myFinalExecution);
+/*********************************************************************
+ * Above is code for experimenting to understand how waterfall behaves
+ * --------------------------------END================================
+ *********************************************************************/
