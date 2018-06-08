6a0e611c02d5d5d54c34236c400d04c1c2dac062
Shiffler <ajshiff@byui.edu>
2018-06-08 13:43:12 -0600

    added debugs to e00, created functional example in e01, but no solution to the problem yet

--- async-you_e00.js
+++ async-you_e00.js
@@ -5,7 +5,7 @@ var async = require('async');
 const http = require('http');
 const fs = require('fs');
 const filepath = process.argv[2];
-
+/*
 async.waterfall([
     function (callback) {
         callback(null, 'one', 'two');
@@ -21,6 +21,7 @@ async.waterfall([
 ], function (err, result) {
     // result now equals 'done'
 });
+*/
 
 // Or, with named functions:
 async.waterfall([
@@ -28,19 +29,26 @@ async.waterfall([
     mySecondFunction,
     myLastFunction,
 ], function (err, result) {
+    console.log(result);
     // result now equals 'done'
 });
 
 function myFirstFunction(callback) {
+    setTimeout(function () {
+        console.log('START');
+    }, 3000);
     callback(null, 'one', 'two');
 }
 
 function mySecondFunction(arg1, arg2, callback) {
     // arg1 now equals 'one' and arg2 now equals 'two'
+    console.log(arg1, arg2);
+    //setTimeout(callback(null, 'three'), 3000);
     callback(null, 'three');
 }
 
 function myLastFunction(arg1, callback) {
     // arg1 now equals 'three'
+    console.log(arg1);
     callback(null, 'done');
 }
--- async-you_e01.js
+++ async-you_e01.js
@@ -2,13 +2,39 @@
  * WATERFALL
  **********************************************************/
 var async = require('async');
-const http = require('http');
-const fs = require('fs');
-const filepath = process.argv[2];
-console.log(filepath);
+// const http = require('http');
+// const fs = require('fs');
+// const filepath = process.argv[2];
+// console.log(filepath);
 
+var one = 'one',
+    two = 'two';
 
+function myFristFunction(callback) {
+    console.log('1: Start');
+    callback(null, one, two);
+}
 
+function mySecondFunction(won, too, callback) {
+    console.log('2 won: ' + won);
+    console.log('2 too: ' + too);
+    let wontoo = won + too;
+    callback(null, wontoo);
+}
+
+function myThirdFunction(juan, callback) {
+    console.log('3 juan: ' + juan);
+    callback(null, juan);
+}
+
+function myFinalExecution(err, finalOutput) {
+    if (err) {
+        console.log('error: ' + err);
+    }
+    console.log('Result: ' + finalOutput);
+}
+
+/*
 async.waterfall([
     function (callback) {
         // let url = '';
@@ -25,3 +51,10 @@ async.waterfall([
     }
 
 ]);
+*/
+
+async.waterfall([
+    myFristFunction,
+    mySecondFunction,
+    myThirdFunction
+], myFinalExecution);
