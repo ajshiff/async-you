c0f222cd2fe996ec35771b0169da53ebec2b7de9
Shiffler <ajshiff@byui.edu>
2018-06-08 20:03:46 -0600

    Logic in place for _e02

--- async-you_e02.js
+++ async-you_e02.js
@@ -1,3 +1,32 @@
 /**********************************************************
- * 
+ * SERIES OBJECT
  **********************************************************/
+// INITIALIZE LIBRARIES, DECLARE GLOBAL VARIABLES
+const async = require('async');
+const http = require('http');
+const fs = require('fs');
+const url1 = process.argv[2];
+const url2 = process.argv[3];
+
+// HTTP.GET ASYNC FUNCTION
+const httpGetFrom = function (url, manager) {
+    http.get(url, function (response) {
+        var dataCollector = '';
+        response.setEncoding('utf8').on('data', function (data) {
+            dataCollector += data;
+        });
+        response.on('end', function () {
+            //console.log(dataCollector)
+            manager(null, dataCollector);
+        });
+    });
+};
+
+const printResults = function (results) {
+    console.log(results);
+};
+
+async.series({
+    requestOne: httpGetFrom(url1),
+    requestTwo: httpGetFrom(url2)
+}, printResults);
