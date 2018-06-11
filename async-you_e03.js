217d889f1cc3abc7cc10dfa7e5d19c66995bf151
Aaron Shiffler <ajshiffler@gmail.com>
2018-06-11 17:57:01 -0600

    e03 complete and compared with given solution

--- async-you_e03.js
+++ async-you_e03.js
@@ -1,3 +1,40 @@
 /**********************************************************
- * 
+ * ASYNC EACH
  **********************************************************/
+const http = require('http');
+const async = require('async');
+const url1 = process.argv[2];
+const url2 = process.argv[3];
+var urls = [url1, url2];
+
+const httpGetRequest = function (url, manager) {
+    http.get(url, function (response) {}).on('error', function (err) {
+        manager(err);
+    });
+};
+
+const logErrors = function (err, result) {
+    if (err) console.error(err);
+    else console.log('SUCCESS! ' + result);
+};
+
+async.each(urls, httpGetRequest, logErrors);
+
+// ASYNC-YOU's GIVEN SOULTION
+// var http = require('http'),
+//     async = require('async');
+
+// async.each(process.argv.slice(2), function (item, done) {
+//         http.get(item, function (res) {
+//             res.on('data', function (chunk) {});
+
+//             res.on('end', function () {
+//                 done(null);
+//             });
+//         }).on('error', function (err) {
+//             done(err);
+//         });
+//     },
+//     function (err) {
+//         if (err) console.error('THIS COUNTS AS AN ERRoR:' + err);
+//     });
