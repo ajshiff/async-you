ec36f7d430910a1ce00a2b555aa17fc761a18893
Aaron Shiffler <ajshiffler@gmail.com>
2018-06-11 18:00:38 -0600

    Added Comments to my solution

--- async-you_e03.js
+++ async-you_e03.js
@@ -1,23 +1,31 @@
 /**********************************************************
  * ASYNC EACH
  **********************************************************/
+// DECLARE NEEDED VARIABLES AND INLCUDE NEEDED LIBRARIES
 const http = require('http');
 const async = require('async');
 const url1 = process.argv[2];
 const url2 = process.argv[3];
-var urls = [url1, url2];
 
+// A COLLECTION OF URLS TO SEND REQUESTS TO
+var urls = [url1, url2]; // OR
+// var urls = process.argv.slice(2)
+
+
+// FUNCTION TO SEND HTTP GET REQUEST
 const httpGetRequest = function (url, manager) {
     http.get(url, function (response) {}).on('error', function (err) {
         manager(err);
     });
 };
 
+// FUNCTION TO LOG EACH REQUEST
 const logErrors = function (err, result) {
     if (err) console.error(err);
     else console.log('SUCCESS! ' + result);
 };
 
+// ASYNC EACH, MAIN DRIVER OF SCRIPT
 async.each(urls, httpGetRequest, logErrors);
 
 // ASYNC-YOU's GIVEN SOULTION
