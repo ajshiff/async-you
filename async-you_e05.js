29833840a2da3788cc0c6f9ed1cc9434346c0707
Shiffler <ajshiff@byui.edu>
2018-06-13 15:15:28 -0600

    A little closer to the solution

--- async-you_e05.js
+++ async-you_e05.js
@@ -1,3 +1,35 @@
 /**********************************************************
- * 
+ * ASYNC TIMES
  **********************************************************/
+const http = require('http');
+const async = require('async');
+const url = process.argv[2] + process.argv[3];
+// console.log(process.argv[2]);
+// console.log(process.argv[3]);
+
+const httpPostRequest = function (url, manager) {
+    http.request('POST', url, function (response) {
+
+    }).on('error', function (err) {
+        manager(err)
+    });
+}
+
+const httpGetRequest = function (url, manager) {
+    url += extension;
+    http.get(url, function (response) {
+        let dataCollector = '';
+        response.setEncoding('utf8').on('data', function (data) {
+            dataCollector += data;
+        });
+        response.on('end', function () {
+            manager(null, dataCollector);
+        })
+    }).on('error', function (err) {
+        manager(err);
+    });
+};
+
+async.times(5, function (manager) {
+    httpPostRequest(url, manager)
+}, httpGetRequest)
