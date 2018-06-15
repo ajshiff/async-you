01e9d59c144f80e07142bff41a2f5629d6b03b89
Shiffler <ajshiff@byui.edu>
2018-06-14 20:16:58 -0600

    Logic in place for solution, just needs some troubleshooting

--- async-you_e06.js
+++ async-you_e06.js
@@ -1,3 +1,33 @@
 /**********************************************************
- * 
+ * REDUCE
  **********************************************************/
+const http = require('http');
+const async = require('async');
+const url = process.argv[2];
+const dataPacket = ['one', 'two', 'three'];
+
+// console.log(url);
+
+const httpGetRequest = function (query, manager) {
+    query = url + '?' + query;
+    // console.log(query);
+    http.get(query, function (response) {
+        let dataCollector = '';
+        response.setEncoding('utf8');
+        response.on('data', function (data) {
+            // console.log(data);
+            dataCollector += data;
+        });
+        response.on('end', function () {
+            // console.log(dataCollector);
+            manager(null, dataCollector);
+        });
+    });
+};
+
+const printResults = function (err, results) {
+    if (err) console.error(err);
+    else console.log(results);
+};
+
+async.reduce(dataPacket, httpGetRequest, printResults);
